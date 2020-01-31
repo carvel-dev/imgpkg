c799bf54d2d73cb2c23d3d63583733a888019643
Dmitriy Kalinin <cppforlife@gmail.com>
2020-01-31 10:33:37 -0500

    fix version test

--- test/e2e/version_test.go
+++ test/e2e/version_test.go
@@ -8,7 +8,7 @@ import (
 func TestVersion(t *testing.T) {
 	out := Imgpkg{t, Logger{}}.Run([]string{"version"})
 
-	if !strings.Contains(out, "Client Version") {
+	if !strings.Contains(out, "imgpkg version") {
 		t.Fatalf("Expected to find client version")
 	}
 }
