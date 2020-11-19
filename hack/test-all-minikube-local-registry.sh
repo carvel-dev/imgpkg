cef998dba622fdc32866fc6c5a19e49ee4c58559
Joao Pereira <joaod@vmware.com>
2020-11-19 14:22:50 -0500

    Remove kapp deploy

--- hack/test-all-minikube-local-registry.sh
+++ hack/test-all-minikube-local-registry.sh
@@ -2,8 +2,6 @@
 
 set -e -x -u
 
-kapp deploy -a reg -f test/e2e/assets/minikube-local-registry.yml -y
-
 export IMGPKG_E2E_IMAGE="localhost:5000/minikube-tests/test-repo"
 export IMGPKG_E2E_RELOCATION_REPO="localhost:5000/minikube-tests/test-relocation-repo"
 ./hack/test-all.sh $@
