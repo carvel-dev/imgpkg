d37fe23c9a0c4aca106066fa1df5f41e4d467123
Steven Locke <slocke@pivotal.io>
2020-12-08 16:51:36 -0800

    Update the file excluding flag to "file-base-exclude"
    * Deprecate the old flag
    * Change the wording of the help text
    * Add a test to ensure it works

--- pkg/imgpkg/cmd/file_flags.go
+++ pkg/imgpkg/cmd/file_flags.go
@@ -10,11 +10,14 @@ import (
 type FileFlags struct {
 	Files []string
 
-	FileExcludeDefaults []string
+	ExcludedFileBasenames []string
 }
 
 func (s *FileFlags) Set(cmd *cobra.Command) {
 	cmd.Flags().StringSliceVarP(&s.Files, "file", "f", nil, "Set file (format: /tmp/foo, -) (can be specified multiple times)")
 
-	cmd.Flags().StringSliceVar(&s.FileExcludeDefaults, "file-exclude-defaults", []string{".git"}, "Excluded file paths by default (can be specified multiple times)")
+	cmd.Flags().StringSliceVar(&s.ExcludedFileBasenames, "file-exclude-defaults", []string{".git"}, "Excluded file paths by default (can be specified multiple times)")
+	cmd.Flags().MarkDeprecated("file-exclude-defaults", "use '--file-base-exclude' instead")
+
+	cmd.Flags().StringSliceVar(&s.ExcludedFileBasenames, "file-base-exclude", []string{".git"}, "Exclude all files whose base name matches (format: bar.yaml) (can be specified multiple times)")
 }
