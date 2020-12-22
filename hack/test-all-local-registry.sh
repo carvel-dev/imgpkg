9ecde093d13a203f9cbf844ede5951aa8d489c97
Joao Pereira <joaod@vmware.com>
2020-12-22 11:13:30 -0500

    Cleanup after itself
    When all tests run locally ensure that the docker registry is always
    removed in case of success or error.
    Also change the port of that registry to be 5001

--- hack/test-all-local-registry.sh
+++ hack/test-all-local-registry.sh
@@ -2,12 +2,16 @@
 
 set -e -x -u
 
-PORT=${1:-5000}
+PORT=${1:-5001}
+
+function cleanup {
+  docker stop registry-"$PORT"
+  docker rm registry-"$PORT"
+}
+trap cleanup EXIT
+
 
 docker run -d -p "$PORT":5000 --restart always --name registry-"$PORT" registry:2
 export IMGPKG_E2E_IMAGE="localhost:$PORT/local-tests/test-repo"
 export IMGPKG_E2E_RELOCATION_REPO="localhost:$PORT/local-tests/test-relocation-repo"
 ./hack/test-all.sh $@
-
-docker stop registry-"$PORT"
-docker rm registry-"$PORT"
