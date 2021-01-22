29e173eeea792e04d4bcf771152cd2983e045d2d
Dennis Leon <leonde@vmware.com>
2021-01-21 16:44:19 -0800

    Test refactor
    - Use go-container registry to create a 'random' layer (instead of creating a layer in our test code)
    - read the copied tarball using imgpks tar reader (instead of having untar logic in our test code)
    
    Authored-by: Dennis Leon <leonde@vmware.com>

--- pkg/imgpkg/cmd/fake_registry_test.go
+++ pkg/imgpkg/cmd/fake_registry_test.go
@@ -2,16 +2,13 @@ package cmd
 
 import (
 	"archive/tar"
-	"bytes"
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/mutate"
-	"github.com/google/go-containerregistry/pkg/v1/partial"
+	"github.com/google/go-containerregistry/pkg/v1/random"
 	"github.com/google/go-containerregistry/pkg/v1/types"
+
 	"github.com/k14s/imgpkg/pkg/imgpkg/image"
 	"github.com/k14s/imgpkg/pkg/imgpkg/image/imagefakes"
 	"io"
@@ -19,7 +16,6 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
-	"time"
 )
 
 type FakeRegistry struct {
@@ -75,6 +71,23 @@ func (r *FakeRegistry) CleanUp() {
 	}
 }
 
+type imageWithTarPath struct {
+	image v1.Image
+	path  string
+	t     *testing.T
+}
+
+func (r *imageWithTarPath) WithNonDistributableLayer() {
+	layer, err := random.Layer(1024, types.OCIUncompressedRestrictedLayer)
+	if err != nil {
+		r.t.Fatalf("unable to create a layer %s", err)
+	}
+	r.image, err = mutate.AppendLayers(r.image, layer)
+	if err != nil {
+		r.t.Fatalf("unable to append a layer %s", err)
+	}
+}
+
 func compress(src string) (*os.File, error) {
 	_, err := os.Stat(src)
 	if err != nil {
@@ -121,80 +134,4 @@ func compress(src string) (*os.File, error) {
 	}
 
 	return tempTarFile, err
-}
-
-type imageWithTarPath struct {
-	image v1.Image
-	path  string
-	t     *testing.T
-}
-
-func (r *imageWithTarPath) WithNonDistributableLayer() {
-	layer, err := Layer(1024, types.OCIUncompressedRestrictedLayer)
-	if err != nil {
-		r.t.Fatalf("unable to create a layer %s", err)
-	}
-	r.image, err = mutate.AppendLayers(r.image, layer)
-	if err != nil {
-		r.t.Fatalf("unable to append a layer %s", err)
-	}
-}
-
-// Layer returns a layer with pseudo-randomly generated content.
-func Layer(byteSize int64, mt types.MediaType) (v1.Layer, error) {
-	fileName := fmt.Sprintf("random_file_%s.txt", time.Now().String())
-
-	// Hash the contents as we write it out to the buffer.
-	var b bytes.Buffer
-	hasher := sha256.New()
-	mw := io.MultiWriter(&b, hasher)
-
-	// Write a single file with a random name and random contents.
-	tw := tar.NewWriter(mw)
-	if err := tw.WriteHeader(&tar.Header{
-		Name:     fileName,
-		Size:     byteSize,
-		Typeflag: tar.TypeRegA,
-	}); err != nil {
-		return nil, err
-	}
-	if _, err := io.CopyN(tw, rand.Reader, byteSize); err != nil {
-		return nil, err
-	}
-	if err := tw.Close(); err != nil {
-		return nil, err
-	}
-
-	h := v1.Hash{
-		Algorithm: "sha256",
-		Hex:       hex.EncodeToString(hasher.Sum(make([]byte, 0, hasher.Size()))),
-	}
-
-	return partial.UncompressedToLayer(&uncompressedLayer{
-		diffID:    h,
-		mediaType: mt,
-		content:   b.Bytes(),
-	})
-}
-
-// uncompressedLayer implements partial.UncompressedLayer from raw bytes.
-type uncompressedLayer struct {
-	diffID    v1.Hash
-	mediaType types.MediaType
-	content   []byte
-}
-
-// DiffID implements partial.UncompressedLayer
-func (ul *uncompressedLayer) DiffID() (v1.Hash, error) {
-	return ul.diffID, nil
-}
-
-// Uncompressed implements partial.UncompressedLayer
-func (ul *uncompressedLayer) Uncompressed() (io.ReadCloser, error) {
-	return ioutil.NopCloser(bytes.NewBuffer(ul.content)), nil
-}
-
-// MediaType returns the media type of the layer
-func (ul *uncompressedLayer) MediaType() (types.MediaType, error) {
-	return ul.mediaType, nil
 }
