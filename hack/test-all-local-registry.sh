243ea40b96f63267de156dd7caa212df17625846
Dennis Leon <leonde@vmware.com>
2021-01-27 13:59:30 -0800

    Test refactor
    - Make it clear what the 'change' from the default registry config is.
    In this case, only the manifest url validation regex needs to be changed
    from the default registry configuration
    
    Authored-by: Dennis Leon <leonde@vmware.com>

--- hack/test-all-local-registry.sh
+++ hack/test-all-local-registry.sh
@@ -2,38 +2,7 @@
 
 set -e -x -u
 
-
 PORT=${1:-5001}
-
-TEMPDIR=$(mktemp -d)
-
-cat > $TEMPDIR/config.yml <<EOF
-version: 0.1
-log:
-  fields:
-    service: registry
-storage:
-  cache:
-    blobdescriptor: inmemory
-  filesystem:
-    rootdirectory: /var/lib/registry
-http:
-  addr: :5000
-  headers:
-    X-Content-Type-Options: [nosniff]
-health:
-  storagedriver:
-    enabled: true
-    interval: 10s
-    threshold: 3
-# Allow foreign layers
-validation:
-  manifests:
-    urls:
-      allow:
-        - ^https?://
-EOF
-
 
 function cleanup {
   docker stop registry-"$PORT"
@@ -41,8 +10,7 @@ function cleanup {
 }
 trap cleanup EXIT
 
-
-docker run -d -p "$PORT":5000 -v $TEMPDIR/config.yml:/etc/docker/registry/config.yml --restart always --name registry-"$PORT" registry:2
+docker run -d -p "$PORT":5000 -e REGISTRY_VALIDATION_MANIFESTS_URLS_ALLOW='- ^https?://' --restart always --name registry-"$PORT" registry:2
 export IMGPKG_E2E_IMAGE="localhost:$PORT/local-tests/test-repo"
 export IMGPKG_E2E_RELOCATION_REPO="localhost:$PORT/local-tests/test-relocation-repo"
 ./hack/test-all.sh $@
