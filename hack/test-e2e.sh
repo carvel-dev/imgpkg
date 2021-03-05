6930592bb76cb7d93e6dc7111e08ec35dee7a787
Joao Pereira <joaod@vmware.com>
2021-03-05 16:08:11 -0500

    Enable richgo if it is installed
    This provides a easier to read output in the command line when running
    tests.
    richgo will only be used if it is present in the PATH if not, it uses
    the normal go output

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -4,6 +4,12 @@ set -e -x -u
 
 go clean -testcache
 
-go test ./test/e2e/ -timeout 60m -test.v $@
+GO=go
+if command -v richgo &> /dev/null
+then
+    GO=richgo
+fi
+
+$GO test ./test/e2e/ -timeout 60m -test.v $@
 
 echo E2E SUCCESS
--- hack/test-perf.sh
+++ hack/test-perf.sh
@@ -4,6 +4,12 @@ set -e -x -u
 
 go clean -testcache
 
-go test ./test/perf/ -timeout 60m -test.v $@
+GO=go
+if command -v richgo &> /dev/null
+then
+    GO=richgo
+fi
+
+$GO test ./test/perf/ -timeout 60m -test.v $@
 
 echo PERF SUCCESS
