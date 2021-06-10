40d29188bd6482a720b93ce4df3373c68a8f26e4
Dmitriy Kalinin <dkalinin@vmware.com>
2021-06-10 19:48:44 -0400

    do not include dir size in tar header as it is not deterministic
    there is no need to include it at all since tar archive does not need for correct functioning

--- test/e2e/deterministic_push_test.go
+++ test/e2e/deterministic_push_test.go
@@ -20,6 +20,9 @@ func TestDeterministicPush(t *testing.T) {
 	out := imgpkg.Run([]string{"push", "--tty", "-i", env.Image + ":tag1", "-f", assetsPath})
 	tag1Digest := helpers.ExtractDigest(t, out)
 
+	// This expected digest should be the same regardless which OS imgpkg runs on
+	require.Equal(t, tag1Digest, "sha256:ceef30cbdce418efde0284f446df9cec9e535adcd6e1010dad30ddae1dc9367b", "Digest should match in all environments")
+
 	out = imgpkg.Run([]string{"push", "--tty", "-i", env.Image + ":tag2", "-f", assetsPath})
 	tag2Digest := helpers.ExtractDigest(t, out)
 
