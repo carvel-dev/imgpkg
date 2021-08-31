421bcc732fbcdb08b40f095189852f291a1461c3
Dennis Leon <leonde@vmware.com>
2021-08-31 13:39:44 -0700

    add debug printing
    Authored-by: Dennis Leon <leonde@vmware.com>

--- pkg/imgpkg/util/retry.go
+++ pkg/imgpkg/util/retry.go
@@ -24,20 +24,24 @@ func Retry(doFunc func() error) error {
 	for i := 0; i < 5; i++ {
 		lastErr = doFunc()
 		if lastErr == nil {
+			if i > 0 {
+				return fmt.Errorf(fmt.Sprintf("This took %d attempts for it to succeed", i))
+			}
 			return nil
 		}
 
 		if tranErr, ok := lastErr.(*transport.Error); ok {
 			if len(tranErr.Errors) > 0 {
 				if tranErr.Errors[0].Code == transport.UnauthorizedErrorCode {
-					return fmt.Errorf("Non-retryable error: %s", lastErr)
+					return fmt.Errorf(fmt.Sprintf("Non-retryable error: %s. attempted %d", lastErr, i))
 				}
 			}
 		}
 		if nonRetryableError, ok := lastErr.(NonRetryableError); ok {
-			return nonRetryableError
+			return fmt.Errorf(fmt.Sprintf("Non-retryable error: %s. attempted %d", nonRetryableError, i))
 		}
 
+		println(fmt.Sprintf("RETRYING %d", i))
 		time.Sleep(1 * time.Second)
 	}
 	return fmt.Errorf("Retried 5 times: %s", lastErr)
