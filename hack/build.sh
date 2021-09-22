d00081426f4721a77c6501a1fafdb3d894d8a1e3
Dennis Leon <leonde@vmware.com>
2021-09-21 18:00:40 -0700

    Disable defaultDockerConfigProvider
    - This interferes with the ordering of auth imgpkg uses.
    -- Specifically, it will prefer the docker config.json file over the
    auth cli flags.
    
    - There doesn't seem to be a nice way to configure / turn this provider
    off. Patching the vendor directory. Will try to make the change
    upstream.
    
    Authored-by: Dennis Leon <leonde@vmware.com>

--- hack/build.sh
+++ hack/build.sh
@@ -10,6 +10,12 @@ go fmt ./cmd/... ./pkg/... ./test/...
 go mod vendor
 go mod tidy
 
+# related to https://github.com/vmware-tanzu/carvel-imgpkg/pull/255
+# there doesn't appear to be a simple way to disable the defaultDockerConfigProvider
+# Having defaultDockerConfigProvider enabled by default results in the imgpkg auth ordering not working correctly
+# Specifically, the docker config.json is loaded before cli flags (and maybe even IaaS metadata services)
+git apply ./hack/patch-k8s-pkg-credentialprovider.patch
+
 # export GOOS=linux GOARCH=amd64
 go build -ldflags="$LDFLAGS" -trimpath -o imgpkg ./cmd/imgpkg/...
 ./imgpkg version
