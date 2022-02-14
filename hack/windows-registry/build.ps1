490fd5b17fb85e11a65ca0c17d8539b111a625b5
Joao Pereira <joaod@vmware.com>
2022-02-14 10:37:36 -0600

    Correct the image ref
    Signed-off-by: Joao Pereira <joaod@vmware.com>

--- hack/windows-registry/build.ps1
+++ hack/windows-registry/build.ps1
@@ -1,4 +1,4 @@
 Write-Host Building registry binary and image
 $version=$(select-string -Path Dockerfile -Pattern "ENV DISTRIBUTION_VERSION").ToString().split()[-1].SubString(1)
-docker build -t ghcr.io/tanzu-vmware/vmware-tanzu/carvel-imgpkg/registry-windows .
-docker tag ghcr.io/tanzu-vmware/vmware-tanzu/carvel-imgpkg/registry-windows:latest ghcr.io/tanzu-vmware/vmware-tanzu/carvel-imgpkg/registry-windows:$version
+docker build -t ghcr.io/vmware-tanzu/carvel-imgpkg/registry-windows .
+docker tag ghcr.io/vmware-tanzu/carvel-imgpkg/registry-windows:latest ghcr.io/vmware-tanzu/carvel-imgpkg/registry-windows:$version
