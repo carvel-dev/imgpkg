b683748e1f7da4b8ede0db1932c02c3c98a23659
Víctor Martínez Bevià <vicmarbev@gmail.com>
2022-04-22 08:42:52 +0200

    Add comments to new functions

--- pkg/imgpkg/cmd/lock_output_flags.go
+++ pkg/imgpkg/cmd/lock_output_flags.go
@@ -11,11 +11,13 @@ type LockOutputFlags struct {
 	LockFilePath string
 }
 
+// SetOnCopy Sets the lock-output flag for Copy command
 func (l *LockOutputFlags) SetOnCopy(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&l.LockFilePath, "lock-output", "",
 		"Location to output the generated lockfile. Option only available when using --bundle or --lock flags")
 }
 
+// SetOnPush Sets the lock-output flag for Push command
 func (l *LockOutputFlags) SetOnPush(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&l.LockFilePath, "lock-output", "",
 		"Location to output the generated lockfile. Option only available when using --bundle flag")
