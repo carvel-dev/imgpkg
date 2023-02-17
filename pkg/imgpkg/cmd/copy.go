shaheerkootteeri/carvel-imgpkg

Allow copy and describe of SBOM, attestations and signatures from cosign
Rename flags to `cosign-artifacts`
Change logic to allow the retrieval of SBOM and attestations as well as
signatures for these images

Signed-off-by: Joao Pereira <joaod@vmware.com>

pkg/imgpkg/cmd/copy.go
@@ -10,13 +10,13 @@ import (
 	regv1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/spf13/cobra"
+	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/artifacts"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/bundle"
 	ctlimgset "github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/imageset"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/internal/util"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/lockconfig"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/plainimage"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/registry"
-	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/signature"
 )
 
 const rootBundleLabelKey string = "dev.carvel.imgpkg.copy.root-bundle"
@@ -30,7 +30,7 @@ type CopyOptions struct {
 	LockOutputFlags LockOutputFlags
 	TarFlags        TarFlags
 	RegistryFlags   RegistryFlags
-	SignatureFlags  SignatureFlags
+	ArtifactsFlags  ArtifactFlags
 
 	RepoDst string
 
@@ -79,7 +79,7 @@ func NewCopyCmd(o *CopyOptions) *cobra.Command {
 	o.LockOutputFlags.SetOnCopy(cmd)
 	o.TarFlags.Set(cmd)
 	o.RegistryFlags.Set(cmd)
-	o.SignatureFlags.Set(cmd)
+	o.ArtifactsFlags.Set(cmd)
 	cmd.Flags().StringVar(&o.RepoDst, "to-repo", "", "Location to upload assets")
 	cmd.Flags().IntVar(&o.Concurrency, "concurrency", 5, "Concurrency")
 	cmd.Flags().BoolVar(&o.IncludeNonDistributable, "include-non-distributable-layers", false,
@@ -118,11 +118,11 @@ func (c *CopyOptions) Run() error {
 	imageSet := ctlimgset.NewImageSet(c.Concurrency, prefixedLogger, tagGen)
 	tarImageSet := ctlimgset.NewTarImageSet(imageSet, c.Concurrency, prefixedLogger)
 
-	var signatureRetriever SignatureRetriever
-	if c.SignatureFlags.CopyCosignSignatures {
-		signatureRetriever = signature.NewSignatures(signature.NewCosign(reg), c.Concurrency)
+	var artifactRetriever ArtifactRetriever
+	if c.ArtifactsFlags.CopyCosignSignatures {
+		artifactRetriever = artifacts.NewArtifacts(artifacts.NewCosign(reg), c.Concurrency)
 	} else {
-		signatureRetriever = signature.NewNoop()
+		artifactRetriever = artifacts.NewNoop()
 	}
 
 	repoSrc := CopyRepoSrc{
@@ -133,11 +133,11 @@ func (c *CopyOptions) Run() error {
 		IncludeNonDistributable: c.IncludeNonDistributable,
 		Concurrency:             c.Concurrency,
 
-		ui:                 levelLogger,
-		registry:           registry.NewRegistryWithProgress(reg, imagesUploaderLogger),
-		imageSet:           imageSet,
-		tarImageSet:        tarImageSet,
-		signatureRetriever: signatureRetriever,
+		ui:                levelLogger,
+		registry:          registry.NewRegistryWithProgress(reg, imagesUploaderLogger),
+		imageSet:          imageSet,
+		tarImageSet:       tarImageSet,
+		artifactRetriever: artifactRetriever,
 	}
 
 	switch {

pkg/imgpkg/cmd/copy_repo_src_test.go
@@ -53,11 +53,11 @@ func TestMain(m *testing.M) {
 	imageSet := imageset.NewImageSet(1, confUI, tagGen)
 
 	subject = CopyRepoSrc{
-		ui:                 uiLogger,
-		imageSet:           imageSet,
-		tarImageSet:        imageset.NewTarImageSet(imageSet, 1, confUI),
-		Concurrency:        1,
-		signatureRetriever: &fakeSignatureRetriever{},
+		ui:                uiLogger,
+		imageSet:          imageSet,
+		tarImageSet:       imageset.NewTarImageSet(imageSet, 1, confUI),
+		Concurrency:       1,
+		artifactRetriever: &fakeSignatureRetriever{},
 	}
 
 	os.Exit(m.Run())
@@ -1172,7 +1172,7 @@ func (f fakeSignatureRetriever) Fetch(images *imageset.UnprocessedImageRefs) (*i
 	return imageset.NewUnprocessedImageRefs(), nil
 }
 
-var _ SignatureRetriever = new(fakeSignatureRetriever)
+var _ ArtifactRetriever = new(fakeSignatureRetriever)
 
 func assertTarballContainsEveryLayer(t *testing.T, imageTarPath string) {
 	path := imagetar.NewTarReader(imageTarPath)

test/helpers/assets.go
@@ -16,6 +16,7 @@ import (
 type Assets struct {
 	T              *testing.T
 	CreatedFolders []string
+	TmpFolder      string
 }
 
 func (a Assets) SimpleAppDir() string {
@@ -88,6 +89,19 @@ func (a *Assets) CreateTempFolder(prefix string) string {
 	require.NoError(a.T, err, "creating bundle folder")
 	a.CreatedFolders = append(a.CreatedFolders, rDir)
 	return rDir
+}
+
+// CreateTempFile Creates a file in a temporary folder that will be deleted when the test execution end
+func (a *Assets) CreateTempFile(prefix string) *os.File {
+	a.T.Helper()
+
+	if a.TmpFolder == "" {
+		a.TmpFolder = a.CreateTempFolder("main-tmp-folder")
+	}
+
+	f, err := ioutil.TempFile(a.TmpFolder, prefix)
+	require.NoError(a.T, err, "creating temporary file in: %s", a.TmpFolder)
+	return f
 }
 
 func (a *Assets) CleanCreatedFolders() {

carvel-dev/imgpkg

pkg/imgpkg/cmd/copy.go
@@ -8,13 +8,13 @@ import (
 
 	"github.com/cppforlife/go-cli-ui/ui"
 	"github.com/spf13/cobra"
+	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/artifacts"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/bundle"
 	ctlimgset "github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/imageset"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/internal/util"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/lockconfig"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/plainimage"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/registry"
-	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/signature"
 )
 
 const rootBundleLabelKey string = "dev.carvel.imgpkg.copy.root-bundle"
@@ -28,7 +28,7 @@ type CopyOptions struct {
 	LockOutputFlags LockOutputFlags
 	TarFlags        TarFlags
 	RegistryFlags   RegistryFlags
-	SignatureFlags  SignatureFlags
+	ArtifactsFlags  ArtifactFlags
 
 	RepoDst string
 
@@ -77,7 +77,7 @@ func NewCopyCmd(o *CopyOptions) *cobra.Command {
 	o.LockOutputFlags.SetOnCopy(cmd)
 	o.TarFlags.Set(cmd)
 	o.RegistryFlags.Set(cmd)
-	o.SignatureFlags.Set(cmd)
+	o.ArtifactsFlags.Set(cmd)
 	cmd.Flags().StringVar(&o.RepoDst, "to-repo", "", "Location to upload assets")
 	cmd.Flags().IntVar(&o.Concurrency, "concurrency", 5, "Concurrency")
 	cmd.Flags().BoolVar(&o.IncludeNonDistributable, "include-non-distributable-layers", false,
@@ -116,11 +116,11 @@ func (c *CopyOptions) Run() error {
 	imageSet := ctlimgset.NewImageSet(c.Concurrency, prefixedLogger, tagGen)
 	tarImageSet := ctlimgset.NewTarImageSet(imageSet, c.Concurrency, prefixedLogger)
 
-	var signatureRetriever SignatureRetriever
-	if c.SignatureFlags.CopyCosignSignatures {
-		signatureRetriever = signature.NewSignatures(signature.NewCosign(reg), c.Concurrency)
+	var artifactRetriever ArtifactRetriever
+	if c.ArtifactsFlags.CopyCosignSignatures {
+		artifactRetriever = artifacts.NewArtifacts(artifacts.NewCosign(reg), c.Concurrency)
 	} else {
-		signatureRetriever = signature.NewNoop()
+		artifactRetriever = artifacts.NewNoop()
 	}
 
 	repoSrc := CopyRepoSrc{
@@ -131,11 +131,11 @@ func (c *CopyOptions) Run() error {
 		IncludeNonDistributable: c.IncludeNonDistributable,
 		Concurrency:             c.Concurrency,
 
-		logger:             levelLogger,
-		registry:           registry.NewRegistryWithProgress(reg, imagesUploaderLogger),
-		imageSet:           imageSet,
-		tarImageSet:        tarImageSet,
-		signatureRetriever: signatureRetriever,
+		logger:            levelLogger,
+		registry:          registry.NewRegistryWithProgress(reg, imagesUploaderLogger),
+		imageSet:          imageSet,
+		tarImageSet:       tarImageSet,
+		artifactRetriever: artifactRetriever,
 	}
 
 	switch {

pkg/imgpkg/cmd/copy_repo_src_test.go
@@ -45,11 +45,11 @@ func TestMain(m *testing.M) {
 	imageSet := imageset.NewImageSet(1, uiLogger, tagGen)
 
 	subject = CopyRepoSrc{
-		logger:             uiLogger,
-		imageSet:           imageSet,
-		tarImageSet:        imageset.NewTarImageSet(imageSet, 1, uiLogger),
-		Concurrency:        1,
-		signatureRetriever: &fakeSignatureRetriever{},
+		logger:            uiLogger,
+		imageSet:          imageSet,
+		tarImageSet:       imageset.NewTarImageSet(imageSet, 1, uiLogger),
+		Concurrency:       1,
+		artifactRetriever: &fakeSignatureRetriever{},
 	}
 
 	os.Exit(m.Run())
@@ -1240,7 +1240,7 @@ func (f fakeSignatureRetriever) Fetch(images *imageset.UnprocessedImageRefs) (*i
 	return imageset.NewUnprocessedImageRefs(), nil
 }
 
-var _ SignatureRetriever = new(fakeSignatureRetriever)
+var _ ArtifactRetriever = new(fakeSignatureRetriever)
 
 func assertTarballContainsEveryLayer(t *testing.T, imageTarPath string) {
 	path := imagetar.NewTarReader(imageTarPath)

test/helpers/assets.go
@@ -15,6 +15,7 @@ import (
 type Assets struct {
 	T              *testing.T
 	CreatedFolders []string
+	TmpFolder      string
 }
 
 func (a Assets) SimpleAppDir() string {
@@ -87,6 +88,19 @@ func (a *Assets) CreateTempFolder(prefix string) string {
 	require.NoError(a.T, err, "creating bundle folder")
 	a.CreatedFolders = append(a.CreatedFolders, rDir)
 	return rDir
+}
+
+// CreateTempFile Creates a file in a temporary folder that will be deleted when the test execution end
+func (a *Assets) CreateTempFile(prefix string) *os.File {
+	a.T.Helper()
+
+	if a.TmpFolder == "" {
+		a.TmpFolder = a.CreateTempFolder("main-tmp-folder")
+	}
+
+	f, err := os.CreateTemp(a.TmpFolder, prefix)
+	require.NoError(a.T, err, "creating temporary file in: %s", a.TmpFolder)
+	return f
 }
 
 func (a *Assets) CleanCreatedFolders() {