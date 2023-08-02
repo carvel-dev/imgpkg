5011eec9648210140d91eabf5ca7ebb61a977720
Joe Searcy <joe@twr.io>
2023-08-02 14:33:35 -0400

    Add comments and address some PR feedback
    Signed-off-by: Joe Searcy <joe@twr.io>

--- pkg/imgpkg/cmd/label_flags.go
+++ pkg/imgpkg/cmd/label_flags.go
@@ -7,10 +7,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// LabelFlags is a struct that holds the labels for an OCI artifact
 type LabelFlags struct {
 	Labels map[string]string
 }
 
+// Set sets the labels for an OCI artifact
 func (l *LabelFlags) Set(cmd *cobra.Command) {
-	cmd.Flags().StringToStringVarP(&l.Labels, "labels", "l", make(map[string]string), "Set labels on image")
+	cmd.Flags().StringToStringVarP(&l.Labels, "labels", "l", map[string]string{}, "Set labels on image")
 }
