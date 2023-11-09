5f7c87395c376240a3e716c9bdfbd4094df63a02
ashpect <ashishndiitr@gmail.com>
2023-11-09 17:00:19 +0530

    Parsed ociformat to ImageOrIndex struct

--- pkg/imgpkg/imagetar/tar_oci_writer.go
+++ pkg/imgpkg/imagetar/tar_oci_writer.go
@@ -9,6 +9,7 @@ import (
 
 	regv1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/layout"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/vmware-tanzu/carvel-imgpkg/pkg/imgpkg/imagedesc"
 )
 
@@ -16,21 +17,44 @@ type MyImageIndex struct {
 	Index regv1.ImageIndex
 }
 
-func (mi *MyImageIndex) Ref() string {
+func (mi MyImageIndex) Ref() string {
 	return "my-image-ref"
 }
 
-func (mi *MyImageIndex) Tag() string {
+func (mi MyImageIndex) Tag() string {
 	return "latest"
 }
 
-func (mi *MyImageIndex) Digest() (regv1.Hash, error) {
-	return regv1.Hash{}, nil
+func (mi MyImageIndex) MediaType() (types.MediaType, error) {
+	return mi.Index.MediaType()
 }
 
-func lmao() {
-	fmt.Println("jojop")
+func (mi MyImageIndex) Digest() (regv1.Hash, error) {
+	return mi.Index.Digest()
 }
+
+func (mi MyImageIndex) Size() (int64, error) {
+	return mi.Index.Size()
+}
+
+func (mi MyImageIndex) IndexManifest() (*regv1.IndexManifest, error) {
+	return mi.Index.IndexManifest()
+}
+
+func (mi MyImageIndex) RawManifest() ([]byte, error) {
+	return mi.Index.RawManifest()
+}
+
+func (mi MyImageIndex) Image(h regv1.Hash) (regv1.Image, error) {
+	return mi.Index.Image(h)
+}
+
+func (mi MyImageIndex) ImageIndex(h regv1.Hash) (regv1.ImageIndex, error) {
+	return mi.Index.ImageIndex(h)
+}
+
+// To explicitely implement the ImageIndex interface
+var _ regv1.ImageIndex = MyImageIndex{}
 
 func (r TarReader) ReadOci() ([]imagedesc.ImageOrIndex, error) {
 
@@ -49,65 +73,51 @@ func (r TarReader) ReadOci() ([]imagedesc.ImageOrIndex, error) {
 
 	//FromPath checks for index.json but does not check for oci-layout, so add a check for oci-layout here.
 
-	//Get the oci layout rooted in the file system at path
+	//Get the oci layout rooted in the file system at path, layout index struct
 	l, err := layout.FromPath(r.path)
 	if err != nil {
 		return nil, err
 	}
-	//a, err := l.ImageIndex()
+	ImageIndex, err := l.ImageIndex()
 
 	myImageIndex := MyImageIndex{
-		Index: nil,
+		Index: ImageIndex,
 	}
 
-	var i imagedesc.ImageIndexWithRef
+	var i imagedesc.ImageIndexWithRef = myImageIndex
 
-	i = &myImageIndex
-
-	// imageOrIndex := imagedesc.ImageOrIndex{
-	// 	Image: nil,
-	// 	Index: i,
-	// 	Labels: map[string]string{
-	// 		"label1": "value1",
-	// 		"label2": "value2",
-	// 	},
-	// 	OrigRef: "original-reference",
-	// }
+	imageOrIndex := imagedesc.ImageOrIndex{
+		Image: nil,
+		Index: &i,
+		Labels: map[string]string{
+			"label1": "value1",
+			"label2": "value2",
+		},
+		OrigRef: "original-reference",
+	}
 
 	ref := imageOrIndex.Ref()
 	fmt.Println("Ref:", ref)
 
-	//Conversion from layout.ImageIndex to imageOrIndex
-	//var imgOrIndexes imagedesc.ImageOrIndex
-	ImageIndex, err := l.ImageIndex()
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("ImageIndex:", ImageIndex)
-
-	// var myImageIndex imagedesc.ImageIndexWithRef = &MyImageIndex{
-	// 	Image:   someImage,    // Replace with an actual ImageIndex instance
-	// 	Index:   someIndex,    // Replace with an actual ImageIndex instance
-	// 	SomeRef: "defaultRef", // Set the default reference string
-	// 	SomeTag: "defaultTag", // Set the default tag string
-	// }
-
-	//imgOrIndexes.Index = &ImageIndex
+	//add imageOrIndex to the slice of imageOrIndex
+	var imageOrIndexSlice []imagedesc.ImageOrIndex
+	imageOrIndexSlice = append(imageOrIndexSlice, imageOrIndex)
 
 	//imgOrIndexes = append(imgOrIndexes, imagedesc.ImageOrIndex{Index: ImageIndex})
 	//Handle multiples cases when manifests in index.json are >1
 
 	//IMP
-	//img, err := loadImage(path, false)
 	//ok is the bool that tells us if the image is an image or an index
 	//t is the v1.Image or v1.ImageIndex
-	// t, _ := img.(v1.Image)
-
+	// t, _ := ImageIndex.(v1.Image)
 	// if err != nil {
 	// 	return nil, err
 	// }
 
-	// crane.SaveOCI(t, "/Users/ashishkumarsingh/Desktop/stuff/ashpect/imgpkg/cmd/imgpkg/hotstuff")
+	//print the slice
+	fmt.Println(imageOrIndexSlice)
 
-	return nil, nil
+	//crane.SaveOCI(t, "/Users/ashishkumarsingh/Desktop/stuff/ashpect/imgpkg/cmd/imgpkg/hotstuff")
+
+	return imageOrIndexSlice, nil
 }
