2821cdea4f4952f153b67b84e417a1da0972a433
Unnati Mishra <unnati.mishra@broadcom.com>
2026-01-20 16:09:17 +0530

    Remove govet as default linter checks it
    Signed-off-by: Unnati Mishra <unnati.mishra@broadcom.com>
    
    Remove close function
    
    Signed-off-by: Unnati Mishra <unnati.mishra@broadcom.com>

--- pkg/imgpkg/v1/copy_test.go
+++ pkg/imgpkg/v1/copy_test.go
@@ -330,9 +330,6 @@ func TestToTarImage(t *testing.T) {
 		require.NoError(t, err)
 		require.Greater(t, len(layersInTar), 1)
 		require.NotContains(t, layersInTar, failedDigest, "tar should not contain the layer that fails to download")
-		if closer, ok := interface{}(&reader).(io.Closer); ok {
-			closer.Close()
-		}
 
 		opts := opts
 		opts.Resume = true
